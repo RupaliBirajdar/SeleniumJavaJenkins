006a30ac645286ec9be683ff653b79790c7f6080
RupaliBirajdar <134680072+RupaliBirajdar@users.noreply.github.com>
2023-05-29 16:31:27 +0530

    Java file uploaded

---
+++ HandleChromeBrowser.java
@@ -0,0 +1,29 @@
+package firstPackage;
+
+
+
+	import org.openqa.selenium.WebDriver;
+	import org.openqa.selenium.chrome.ChromeDriver;
+	import org.testng.annotations.Test;
+
+	public class HandleChromeBrowser 
+	{
+	  @Test
+	  public void f() throws Exception
+	  {
+		  String projectPath = System.getProperty("user.dir");
+		  System.setProperty("webdriver.chrome.driver", projectPath + "\\drivers\\chromedriver.exe");
+		  WebDriver driver = new ChromeDriver();
+		  Thread.sleep(5000);
+		  driver.quit();
+		  
+		  
+		  
+		  
+		  
+		  
+	  }
+	}
+
+
+
